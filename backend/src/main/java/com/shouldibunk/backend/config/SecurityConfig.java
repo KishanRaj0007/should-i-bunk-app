80bbfaa43990cab83fa305657a822c584355f84f
Kishan Raj <kishancode55@gmail.com>
2025-08-01 21:37:38 +0530

    Second backend change

--- backend/src/main/java/com/shouldibunk/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/shouldibunk/backend/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.shouldibunk.backend.config;
 
 import com.shouldibunk.backend.repository.UserRepository;
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -50,9 +49,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://shouldibunkbykishan.netlify.app", "http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://shouldibunkbykishan.netlify.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/api/**", configuration);
